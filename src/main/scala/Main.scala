5e658b08e970b40cd17a8d9f329a374742564b16
Atroa-shaman <73079175+Atroa-shaman@users.noreply.github.com>
2021-12-09 20:20:59 +0100

    GMM method, integration of bets into model

--- src/main/scala/Main.scala
+++
@@ -1,9 +0,0 @@
-package agh
-import scala.io.Source
-import agh.Surface._
-
-object Main extends App {
-  val comparator = new Comparator()
-  //comparator.compare(Array(4920,4913),Clay) //djokovic i murray
-  comparator.compare(Array(4920,4913, 4742, 3819),Clay) //djokovic, murray, nadal, federer
-}
--- src/main/scala/Surface.scala
+++
@@ -1,6 +0,0 @@
-package agh
-
-object Surface extends Enumeration{
-    type Surface = Value
-    val Hard, Grass, Carpet, Clay = Value
-}
